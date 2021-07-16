b73f4c2948bb79b5c25c991415a110c82f474223
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-07-15 19:41:01 -0700

    Set up base urlpatterns

--- project_wishlist/settings.py
+++ project_wishlist/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.wishlist_app',
+    'debug_toolbar',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
